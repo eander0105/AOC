befa9b8469222be7a061f0d1b62d60ffbc61f33d
Emil Andersson <eander0105@gmail.com>
2024-12-10 11:51:19 +0100

    unfinnished business

--- 2024/2/main.go
+++ 2024/2/main.go
@@ -21,11 +21,11 @@ func checkValue(incresing bool, lastValue int64, value int64, maxChange float64)
 	return true
 }
 
-func safeReport(report string) bool {
+func safeReport(report []string) bool {
 	direction := '.'
 	var lastValue int64 = -1
 
-	for _, v := range strings.Fields(report) {
+	for _, v := range report {
 		value, err := strconv.ParseInt(v, 10, 64)
 		if err != nil {
 			return false
@@ -40,7 +40,6 @@ func safeReport(report string) bool {
 					direction = '-'
 				}
 			}
-			// Check if 
 			if !checkValue(direction == '+', lastValue, value, 3) {
 				return false
 			}
@@ -50,21 +49,52 @@ func safeReport(report string) bool {
 	return true
 }
 
-func part1(data []string) {
+func delete_at_index(a []string, i int) []string {
+	copy(a[i:], a[i+1:]) // Shift a[i+1:] left one index.
+	a[len(a)-1] = ""     // Erase last element (write zero value).
+	a = a[:len(a)-1]     // Truncate slice.
+	return a
+}
+
+func part1(data []string) int {
 	safeReports := 0
 	for _, row := range data {
-		check := safeReport(row)
+		check := safeReport(strings.Fields(row))
 		if check {
 			safeReports++
 		}
 	}
-	fmt.Println("Safe reports:", safeReports)
+	return safeReports
 }
 
+func part2(data []string) int {
+	safeReports := 0
+	for _, row := range data {
+		report := strings.Fields(row)
+		fmt.Println(report)
+		for field, _ := range report {
+			if field > 0 {
+				r := []string{}
+				copy(r[:], report)
+				cReport := delete_at_index(r, field)
+				fmt.Println(cReport)
+				
+			}
 
+		}
+
+		break
+		// check := safeReport(row, 1)
+		// fmt.Printf("Report %v, [%s] was %v\n", i ,row, check)
+		// if check {
+		// 	safeReports++
+		// }
+	}
+	return safeReports
+}
 
 func main() {
-	data, err := os.ReadFile("input.txt")
+	data, err := os.ReadFile("test.txt")
 	if err != nil {
 		fmt.Println("no file!")
 		return
@@ -72,5 +102,6 @@ func main() {
 
 	value := strings.Split(string(data), "\n")
 
-	part1(value)
+	fmt.Println("Part1, safe reports:", part1(value))
+	fmt.Println("Part2, safe reports:", part2(value))
 }
